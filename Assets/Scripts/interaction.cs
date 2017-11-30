7264a76aa5a0653bfe87f1035a96f859f4aff433
Lming1 <spear131121@gmail.com>
2017-11-30 18:12:38 +0900

    first commit

---
+++ Assets/Scripts/interaction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interaction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	// interactive action
+	void touchBegan()
+	{
+		
+	}
+
+	void touchEnded()
+	{
+
+	}
+
+	void touchStay()
+	{
+
+	}
+
+	void touchExit()
+	{
+
+	}
+}
