381b3edc04ae795f738f0cd5b3a2dbe3c7df94c7
Lming1 <spear131121@gmail.com>
2017-12-04 18:57:10 +0900

    test

--- Assets/Scripts/interaction.cs
+++ Assets/Scripts/interaction.cs
@@ -3,34 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class interaction : MonoBehaviour {
-
+	public static Color defaultColor;
+	public static Color selectedColor;
+	public static Material mat;
 	// Use this for initialization
 	void Start () {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	// interactive action
-	void touchBegan()
-	{
-		
-	}
+		mat = GetComponent<Renderer> ().material;
 
-	void touchEnded()
-	{
+		mat.SetFloat("_Mode", 2);
+		mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+		mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+		mat.SetInt("_ZWrite", 0);
+		mat.DisableKeyword("_ALPHATEST_ON");
+		mat.EnableKeyword("_ALPHABLEND_ON");
+		mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+		mat.renderQueue = 3000;
 
-	}
+		defaultColor = new Color32 (255, 255, 255, 255);
+		selectedColor = new Color32 (0, 0, 0, 0);
 
-	void touchStay()
-	{
-
+		mat.color = defaultColor;
 	}
 
-	void touchExit()
-	{
+	void touchBegan(){
+		mat.color = selectedColor;
+		Debug.Log ("selected");
+	}
 
+	void touchEnded(){
+		mat.color = defaultColor;
+		Debug.Log ("end");
+	}
+
+	void touchStay(){
+		mat.color = selectedColor;
+		Debug.Log ("stay");
+	}
+
+	void touchExit(){
+		mat.color = defaultColor;
+		Debug.Log ("end");
 	}
 }
