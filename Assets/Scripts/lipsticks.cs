24df598f4a4c51c526209eb217d324dcbbea17bf
ojlee <dldhwn56@gmail.com>
2017-12-05 04:42:45 +0900

    크기/위치/입술머리색변경 완성

---
+++ Assets/Scripts/lipsticks.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lipsticks : MonoBehaviour {
+
+    GameObject lip;
+    public int st_lip_color = 1;
+    public Renderer rend;
+    public Material[] matA = new Material[6];
+    
+    // Use this for initialization
+    void Start () {
+        lip = GameObject.FindGameObjectWithTag("lip");
+        rend = lip.GetComponent<Renderer>();
+        rend.enabled = true;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void touchEnded()
+    {
+        Debug.Log("터치!");
+        if (st_lip_color <= 5)
+        {
+            rend.sharedMaterial = matA[st_lip_color];
+            st_lip_color++;
+        }
+        else
+        {
+            st_lip_color = 1;
+            rend.sharedMaterial = matA[st_lip_color];
+        }
+
+    }
+}
